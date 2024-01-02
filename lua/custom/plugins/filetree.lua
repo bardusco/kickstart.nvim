a497b97fdb7a516c616fc2552e0ca375e48826fa
Danilo Bardusco <danilo@bardusco.com>
2024-01-02 10:46:39 -0300

    enabled preview mode

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,4 +1,5 @@
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+vim.api.nvim_set_keymap('n', '<Tab>', ':Neotree toggle<CR>', { noremap = true, silent = true })
 
 return {
   "nvim-neo-tree/neo-tree.nvim",
@@ -7,8 +8,15 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
+    "3rd/image.nvim",
   },
-  config = function ()
-    require('neo-tree').setup {}
+  config = function()
+    require('neo-tree').setup {
+      window = {
+        mappings = {
+          ["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
+        }
+      }
+    }
   end,
 }
