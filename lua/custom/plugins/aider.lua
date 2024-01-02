503e37b0397e24bc42cca45669e753025ed6d024
Danilo Bardusco <danilo@bardusco.com>
2024-01-02 10:47:11 -0300

    disabled key biddings

--- lua/custom/plugins/aider.lua
+++ lua/custom/plugins/aider.lua
@@ -1,9 +1,11 @@
+vim.api.nvim_set_keymap('n', '<leader>oa', '<cmd>lua AiderOpen()<cr>', { noremap = true, silent = true })
+
 return {
   'joshuavial/aider.nvim',
   config = function()
     require("aider").setup {
       auto_manage_context = true,
-      default_bindings = true
+      default_bindings = false
     }
   end,
 }
