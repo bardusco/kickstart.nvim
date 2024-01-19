2f345b756fb00da30f71204a3c89f5513b5f89e3
Danilo Bardusco <danilo@bardusco.com>
2024-01-19 12:18:15 -0300

    feat(plugins:aider): add visual aid for aider background status in lualine
    A visual indicator has been added to the lualine status line for the aider plugin. It conditionally shows different colors depending on the `_G.aider_background_status` value. When the status is 'idle', a color defined by `#8FBCBB` is shown, and when the status is 'working', the color changes to `#BF616A`. The `aider_idle` and `aider_working` identifiers are expected to be associated with the respective visual representations of the plugin's state. This improves UX by providing immediate visual feedback regarding the background operation status directly in the status line.

--- lua/custom/plugins/aider.lua
+++ lua/custom/plugins/aider.lua
@@ -1,11 +1,27 @@
 vim.api.nvim_set_keymap('n', '<leader>oa', '<cmd>lua AiderOpen()<cr>', { noremap = true, silent = true })
 
+vim.g.lualine_x = {
+  {
+    aider_idle,
+    color = { fg = '#8FBCBB' },
+    cond = function()
+      return _G.aider_background_status == 'idle'
+    end
+  },
+  {
+    aider_working,
+    color = { fg = '#BF616A' },
+    cond = function()
+      return _G.aider_background_status == 'working'
+    end
+  }
+}
 return {
   'joshuavial/aider.nvim',
   config = function()
-    require("aider").setup {
+    require('aider').setup({
       auto_manage_context = true,
       default_bindings = false
-    }
+    })
   end,
 }
