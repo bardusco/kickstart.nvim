7f59129a253ea06c313b16138104d59e142dcfc7
Danilo Bardusco <danilo@bardusco.com>
2024-01-19 12:25:02 -0300

    feat(lua): add flutter-tools plugin configuration
    Introduce the flutter-tools plugin configuration in the Lua custom plugins directory. This new setup includes dependencies on 'plenary.nvim' and 'dressing.nvim', and specifies the initialization settings for Flutter development within Neovim. This includes enabling widget guides, configuring LSP color highlighting for Dart variables with several new options such as background and virtual text color highlighting, tailored for Flutter SDK version 2.10 and above. The configuration aims to enhance Flutter development experience by providing visual aids and better integration with Neovim's LSP features.

---
+++ lua/custom/plugins/flutter-tools.lua
@@ -0,0 +1,25 @@
+return {
+  'akinsho/flutter-tools.nvim',
+  lazy = false,
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'stevearc/dressing.nvim',
+  },
+  config = function()
+    require("flutter-tools").setup {
+      widget_guides = {
+        enabled = true,
+      },
+      lsp = {
+        color = { -- show the derived colours for dart variables
+          enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+          background = false, -- highlight the background
+          background_color = nil, -- required, when background is transparent (i.e. background_color = { r = 19, g = 17, b = 24},)
+          foreground = false, -- highlight the foreground
+          virtual_text = true, -- show the highlight using virtual text
+          virtual_text_str = "■", -- the virtual text character to highlight
+        }
+      }
+    }
+  end,
+}
