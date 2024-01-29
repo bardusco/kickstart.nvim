80bb8e6e8c07cd2856fe6a42afb848ff8769912a
Danilo Bardusco <danilo@bardusco.com>
2024-01-29 15:29:06 -0300

    refactor: split complex `init.lua` into multiple modules
    This commit refactors the neovim `init.lua` configuration file by separating
    its various configurations into multiple modular files. The different areas
    of configuration addressed include options, keybindings, LSP setup,
    Telescope setup, Treesitter setup, and completion setup with nvim-cmp.
    Each of these configurations have been extracted from `init.lua` and
    placed into their respective `lua/*.lua` files.
    
    As a result of this refactoring, the `init.lua` file becomes more
    maintainable and easier to navigate. Contributions to different parts of
    the configuration can now be done in a more isolated fashion, reducing
    the risk of conflicts during collaboration and making the overall
    codebase more structured.
    
    This modular approach also follows the best practices in software
    engineering where separation of concerns is encouraged. It allows users
    of the configuration to more clearly understand the purpose of each
    section, and to focus on one area at a time, thereby enhancing the
    quality of the configuration and the ease of making changes.
    
    Whenever a configuration snippet is related to a particular functionality,
    for instance LSP or completion, the snippet is moved to its corresponding
    file: `lsp-setup.lua` for LSP configurations or `cmp-setup.lua` for
    completion configurations.
    
    Summary of changes:
    - Extracted options into `lua/options.lua`
    - Extracted keybindings into `lua/keymaps.lua`
    - Moved LSP configurations to `lua/lsp-setup.lua`
    - Moved Telescope-specific settings to `lua/telescope-setup.lua`
    - Treesitter configuration is now in `lua/treesitter-setup.lua`
    - Nvim-cmp setup, including snippet and kind icons, is moved to
      `lua/cmp-setup.lua`
    
    With clear-cut boundaries between different aspects of the configuration,
    the `.lua` files can be independently maintained, tested, and reused.

---
+++ lua/cmp-setup.lua
@@ -0,0 +1,133 @@
+-- [[ Configure nvim-cmp ]]
+-- See `:help cmp`
+local cmp = require 'cmp'
+local luasnip = require 'luasnip'
+-- Loading custom snippets
+require('luasnip.loaders.from_vscode').lazy_load({
+  --  paths = { vim.fn.expand('~/.config/nvim/lua/custom/snippets/flutter') }
+})
+
+luasnip.config.setup {}
+
+local kind_icons = {
+  Text = "󰉿",
+  Method = "󰆧",
+  Function = "󰊕",
+  Constructor = "",
+  Field = " ",
+  Variable = "󰀫",
+  Class = "󰠱",
+  Interface = "",
+  Module = "",
+  Property = "󰜢",
+  Unit = "󰑭",
+  Value = "󰎠",
+  Enum = "",
+  Keyword = "󰌋",
+  Snippet = "",
+  Color = "󰏘",
+  File = "󰈙",
+  Reference = "",
+  Folder = "󰉋",
+  EnumMember = "",
+  Constant = "󰏿",
+  Struct = "",
+  Event = "",
+  Operator = "󰆕",
+  TypeParameter = " ",
+  Misc = " ",
+}
+-- find more here: https://www.nerdfonts.com/cheat-sheet
+
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  completion = {
+    completeopt = 'menu,menuone,noinsert',
+  },
+  mapping = cmp.mapping.preset.insert {
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete {},
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_locally_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  },
+  formatting = {
+    fields = { "kind", "abbr", "menu" },
+    expandable_indicator = true,
+    format = function(entry, vim_item)
+      -- Kind icons
+      vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+      -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+      vim_item.menu = ({
+        nvim_lsp = "[LSP]",
+        luasnip = "[Snippet]",
+        buffer = "[Buffer]",
+        path = "[Path]",
+      })[entry.source.name]
+      return vim_item
+    end,
+  },
+  sources = {
+    { name = 'luasnip' },
+    { name = 'path' },
+    { name = 'buffer' },
+    { name = 'nvim_lsp' },
+    { name = 'nvim_lsp_signature_help' }
+  },
+  window = {
+    documentation = {
+      border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+    },
+  },
+  --  view = {
+  --    entries = "native"
+  --  },
+  experimental = {
+    ghost_text = true,
+  },
+
+}
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
+-- vim: ts=2 sts=2 sw=2 et
---
+++ lua/options.lua
@@ -0,0 +1,69 @@
+-- [[ Setting options ]]
+-- See `:help vim.o`
+-- NOTE: You can change these options as you wish!
+
+-- Set highlight on search
+vim.o.hlsearch = true
+
+-- Make line numbers default
+vim.wo.number = true
+
+-- Enable mouse mode
+vim.o.mouse = 'a'
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.o.clipboard = 'unnamedplus'
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
+-- NOTE: You should make sure your terminal supports this
+vim.o.termguicolors = true
+
+vim.o.relativenumber = true
+
+vim.o.cursorline = true
+
+vim.o.colorcolumn = "79"
+
+vim.g.python3_host_prog = '~/.virtualenvs/neovim3/bin/python3'
+
+-- Auto reload files from disk
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  pattern = "*",
+  callback = function()
+    if vim.fn.mode() ~= 'c' then
+      vim.cmd('checktime')
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileChangedShellPost", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_echo({ { "File changed on disk. Buffer reloaded.", "WarningMsg" } }, true, {})
+  end,
+})
+
+vim.o.autoread = true
+
+-- vim: ts=2 sts=2 sw=2 et
