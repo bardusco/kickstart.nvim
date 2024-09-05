b407de611e4ad3f8113d3c8bad41fcd6179f097d
Danilo Bardusco <danilo@bardusco.com>
2024-09-05 11:20:36 -0300

    feat: add diffview.nvim plugin configuration

---
+++ lua/custom/plugins/diffview.lua
@@ -0,0 +1,31 @@
+return {
+  'sindrets/diffview.nvim',
+  event = 'BufRead',
+  dependencies = { 'nvim-lua/plenary.nvim' }, -- Diffview.nvim depende de plenary.nvim
+  config = function()
+    -- Configuração personalizada, se necessário
+    local cb = require('diffview.config').diffview_callback
+
+    require('diffview').setup({
+      diff_binaries = false,   -- Show diffs for binaries
+      enhanced_diff_hl = true, -- See ':h diffview-config-enhanced_diff_hl'
+      use_icons = true,        -- Requires nvim-web-devicons
+      icons = {                -- Only applies when use_icons is true.
+        folder_closed = "",
+        folder_open = "",
+      },
+      signs = {
+        fold_closed = "",
+        fold_open = "",
+        done = "✓",
+      },
+      file_panel = {
+        listing_style = "tree", -- One of 'list' or 'tree'
+        win_config = {          -- See ':h diffview-config-win_config'
+          position = "left",
+          width = 35,
+        },
+      },
+    })
+  end
+}
